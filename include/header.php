b3639d33c1d202cbef2914f62adaa63437589a5d
MarieTheres <mk279@hdm-stuttgart.de>
2018-02-01 01:32:28 +0100

    Header update

--- include/header.php
+++ include/header.php
@@ -11,6 +11,8 @@ while($row = mysqli_fetch_assoc($rest11)) {
     $db_txtid11 = $row['db_txtid'];
     $db_txtcontent11 = $row['db_txtcontent'];
 }
+
+
 print ("
 <html>
 <head>
@@ -39,155 +41,85 @@ print ("
 <link href='https://fonts.googleapis.com/css?family=Raleway' rel='stylesheet'>
 <script src='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js'></script>
 
+<!--Navigationsmenü-->
 
 </head>
-<body background=#fff >
+<link href='https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' />
 
-
-
-<!DOCTYPE html>
-
-<html lang='en'>
-    <head>
-    <meta charset='utf-8'> 
-  <meta name='viewport' content='width=device-width, initial-scale=1'>  
-  <meta name='author' content='sumit kumar'> 
-  <title>Trial</title> 
-  <link href='css/bootstrap.css' rel='stylesheet' type='text/css'>
-  <link href='css/font-awesome.css' rel='stylesheet' type='text/css'>
-  <link href='css/style.css' rel='stylesheet' type='text/css'>
-  <script src='https://use.fontawesome.com/07b0ce5d10.js'></script>
-    </head>
-
-<body>
-    
-
-    
-    
-<!--====================== NAVBAR MENU START===================-->
-    
-  
-<nav class='menu'>
+ <nav class='navbar menubar navbar-fixed-top'>
   <div class='container'>
-    <div class='navbar-header'>
-      <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#myNavbar'>
-        <span class='icon-bar'></span>
-        <span class='icon-bar'></span>
-        <span class='icon-bar'></span>                        
-      </button>
-      <a class='navbar-brand' href='#'><img src='https://lh3.googleusercontent.com/-N4NB2F966TU/WM7V1KYusRI/AAAAAAAADtA/fPvGVNzOkCo7ZMqLI6pPITE9ZF7NONmawCJoC/w185-h40-p-rw/logo.png'></a>
-    </div>
-    <div class='collapse navbar-collapse' id='myNavbar'>
-      <ul class='nav navbar-nav navbar-left'>
-        <li><a href='index.php?sess=$sess&ts=$ts'  onFocus='if (this.blur) this.blur()'class='h4'>Startseite</a></li>
-        
-        <li class='dropdown'>
-          <a class='dropdown-toggle' data-toggle='dropdown' href='#'> Artikel <span class='caret'></span></a>
-          <ul class='dropdown-menu'>
-            <li><a href='#'>Page 1-1</a></li>
-            <li><a href='#'>Page 1-2</a></li>
-            <li><a href='#'>Page 1-3</a></li>
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class='navbar-header'>
+              <button type='button' data-target='#navbarCollapse' data-toggle='collapse' class='navbar-toggle'>
+                  <span class='sr-only'></span>
+                  <span class='icon-bar'></span>
+                  <span class='icon-bar'></span>
+                  <span class='icon-bar'></span>
+              </button>
+              <a href='#' class='navbar-brand logo'><h4>My Logo Here</h4></a>
+          </div>
+   
+
+          <ul class='nav navbar-nav navbar-right rightmenu'>
+              <li><a href='warenkorb.php?sess=$sess&ts=$ts&kat=0&artikel=0&next=0&page=0&back=index&action=view'><span><i class='fa fa-shopping-cart'></i></span></a></li>
+              
+              <li class='dropdown'>
+                <a href='#' class='dropdown-toggle' type='button' data-toggle='dropdown'><span><i class='fa fa-user'></i></span></a>
+                <ul class='dropdown-menu'>
+                  <li><a href='#'>Login</a></li>
+                  <li><a href='#'>Register</a></li>
+                </ul>
+              </li> 
           </ul>
-        </li>
-        
 
-        
+          <div id='navbarCollapse' class='collapse navbar-collapse'>
+              <ul class='nav navbar-nav navbar-right'>
+                  <li class='active'><a href='index.php?sess=$sess&ts=$ts'  onFocus='if (this.blur) this.blur()'>Startseite</a></li>
+                  <li class='dropdown'>
+                  <a id='dLabel' data-toggle='dropdown' href='#'>Shop</a>
+                  <ul class='dropdown-menu' role='menu' aria-labelledby='dLabel'>
+                 <li><a href='https://mars.iuk.hdm-stuttgart.de/~hg034/yourhome/content.php?kat=1&next=0&page=1'>Sofas</a></li>
+                 <li><a href='https://mars.iuk.hdm-stuttgart.de/~hg034/yourhome/content.php?kat=2&next=0&page=1'>Couchtische</a></li>
+                <li><a href='https://mars.iuk.hdm-stuttgart.de/~hg034/yourhome/content.php?kat=3&next=0&page=1'>Lampen</a></li>                            
+                <li><a href='https://mars.iuk.hdm-stuttgart.de/~hg034/yourhome/content.php?kat=4&next=0&page=1'>Stühle</a></li>   
+                <li><a href='https://mars.iuk.hdm-stuttgart.de/~hg034/yourhome/content.php?kat=5&next=0&page=1'>Dekoration</a></li>  
+                  </ul>
+                  </li>
+                  
+          </div>
+  </div><!-- end of container -->    
+</nav>        
+<!--Ende Navigationsmenü-->
 
+<!--Karussell-->
 
-        <li><a href='#'>services</a></li>
-        <li><a href='#'>gallery</a></li>
-        <li><a href='#'>blog</a></li>
-        <li><a href='#'>contact us</a></li>
-      </ul>
-      <form class='navbar-form navbar-right'>
-      <div class='input-group'>        
-        <div class='input-group-btn'>
-          <button class='btn btn-default-1' type='submit'>
-            <i class='glyphicon glyphicon-search'></i>
-          </button>
-        </div>
-    <input type='text' class='form-control' placeholder='Search'>
-      </div>
-          
-        <span class='cart-heart  hidden-sm hidden-xs'> 
-            <a href='#'><i class='fa fa-user' aria-hidden='true'></i></a>
-            <a href='#'><i class='fa fa-cart-plus' aria-hidden='true'></i></a>
-        </span> 
-        <span class='cart-heart  hidden-md hidden-lg'>          
-            <a href='#'><i class='fa fa-heart' aria-hidden='true'></i></a>
-            <a href='#'><i class='fa fa-cart-plus' aria-hidden='true'></i></a>
-            <a href='#'><i class='fa fa-user' aria-hidden='true'></i></a>
-            <a href='#'><i class='fa fa-globe' aria-hidden='true'></i></a>
-            <a href='#'><i class='fa fa-usd' aria-hidden='true'></i></a>
-        </span>   
-    </form>
-        
+<section class='sliderpanel'>
+<header id='myCarousel' class='carousel slide myslider' data-interval='3000' data-ride='carousel'>
+
+<script>
+$('#myCarousel').carousel({interval:3000})
+</script>
+
+    <!-- Carousel indicators -->
+    <ol class='carousel-indicators'>
+        <li data-target='#myCarousel' data-slide-to='0' class='active'></li>
+        <li data-target='#myCarousel' data-slide-to='1'></li>
+    </ol>   
+    <!-- Carousel items -->
+    <div class='carousel-inner'>
+        <div class='item active' style='background-image: url(https://mars.iuk.hdm-stuttgart.de/~hg034/yourhome/img/banner2.jpg);'></div>
+        <div class='item' style='background-image: url(https://mars.iuk.hdm-stuttgart.de/~hg034/yourhome/img/banner1.jpg);'></div>
     </div>
-  </div>
-</nav>
-
-
-
-
-<!--=================CAROUSELE START====================-->
-  
-    <div id='myCarousel' class='carousel slide' data-ride='carousel'>
-  <!-- Indicators -->
-  <ol class='carousel-indicators'>
-    <li data-target='#myCarousel' data-slide-to='0' class='active'></li>
-    <li data-target='#myCarousel' data-slide-to='1'></li>
-    
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class='carousel-inner' role='listbox'>
-    <div class='item active'>
-       <img src='/home/hg034/public_html/yourhome/img/Inspiration.jpg' width='1400px' height='500px'>
-      <div class='carousel-caption hidden-xs'>
-        <h3>Create your Home</h3>
-        <p>New Trends<br> 2018.</p>
-          <button class='btn btn-default'>Angebote</button>
-      </div>
-    </div>
-
-    <div class='item'>
-        <img src='/home/hg034/public_html/yourhome/img/Inspiration.jpg' width='1400px' height='500px'>
-      <div class='carousel-caption hidden-xs'>
-        <h3>Create your Home</h3>
-        <p>New Trends<br> 2018.</p>
-        <button class='btn btn-default'>T</button>
-      </div>
-    </div>
-  </div>
-
-  <!-- Left and right controls -->
-  <a class='left carousel-control' href='#myCarousel' role='button' data-slide='prev'>
-    <span class='glyphicon glyphicon-chevron-left' aria-hidden='true'></span>
-    <span class='sr-only'>Previous</span>
-  </a>
-  <a class='right carousel-control' href='#myCarousel' role='button' data-slide='next'>
-    <span class='glyphicon glyphicon-chevron-right' aria-hidden='true'></span>
-    <span class='sr-only'>Next</span>
-  </a>
+    <!-- Carousel nav -->
+    <a class='carousel-control left' data-slide='prev' href=#myCarousel>‹</a>
+    <a class='carousel-control right' data-slide='next' href='#myCarousel'>›</a>
+</header>
+</section><
 </div>
-    
 
-        
-    
-<script src='js/jquery-3.1.1.js'></script>
-<script src='js/bootstrap.js'></script>
 </body>
 </html>
 
-<table border=0 cellpadding=0 cellspacing=0 id='tableges'>
-<tr><td id='space' height=6>&nbsp;</td></tr>
-</table>
-<table border=0 cellpadding=0 cellspacing=0 id='tableges'>
-<tr>
-<td id='tableft' valign=top>
-<table border=0 cellpadding=0 cellspacing=0 id='tableleft'>
-</body>
-
 ");
 
-?>
+?>?>
